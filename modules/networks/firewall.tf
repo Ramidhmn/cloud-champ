af70eda4ac50272e8a241a6e5e06c83e9bb06236
Ramidhmn <rami.dahmouni@carbon-it.com>
2024-06-12 17:35:03 +0200

    first succeed apply

--- modules/networks/firewall.tf
+++ modules/networks/firewall.tf
@@ -1,23 +1,23 @@
-resource "google_compute_firewall" "default-allow-internal" {
-  name    = "default-allow-internal"
-  network = google_compute_network.my_cloud_ntwrk.name
+# resource "google_compute_firewall" "default-allow-internal" {
+#   name    = "default-allow-internal"
+#   network = google_compute_network.my_cloud_ntwrk.name
 
-  allow {
-    protocol = "icmp"
-  }
+#   allow {
+#     protocol = "icmp"
+#   }
 
-  allow {
-    protocol = "tcp"
-    ports    = ["0-65535"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["0-65535"]
+#   }
 
-  allow {
-    protocol = "udp"
-    ports    = ["0-65535"]
-  }
+#   allow {
+#     protocol = "udp"
+#     ports    = ["0-65535"]
+#   }
 
-  source_ranges = ["10.0.0.0/16"]
-}
+#   source_ranges = ["10.0.0.0/16"]
+# }
 
 
 resource "google_compute_firewall" "deny_all_ext" {
